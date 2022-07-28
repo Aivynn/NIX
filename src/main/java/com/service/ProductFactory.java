fabe86841029ceb96ef5146bef3f1e62bf63cacd
Aivynn <aivyn1337@gmail.com>
2022-07-29 01:15:13 +0300

    hw 6

---
+++ src/main/java/com/service/ProductFactory.java
@@ -0,0 +1,29 @@
+package com.service;
+
+import com.model.Notebook;
+import com.model.Phone;
+import com.model.ProductType;
+import com.model.Smartwatch;
+
+import java.util.Random;
+
+public class ProductFactory {
+    private static final Random RANDOM = new Random();
+
+    private static final ProductService<Phone> PHONE_SERVICE = PhoneService.getInstance();
+    private static final ProductService<Notebook> NOTEBOOK_SERVICE = NotebookService.getInstance();
+    private static final ProductService<Smartwatch>  SMARTWATCH_SERVICE = SmartwatchService.getInstance();
+
+    private ProductFactory() {
+    }
+
+    public static void createAndSave(ProductType type) {
+        switch (type) {
+            case PHONE -> PHONE_SERVICE.createAndSaveProducts(1);
+            case NOTEBOOK -> NOTEBOOK_SERVICE.createAndSaveProducts(1);
+            case SMARTWATCH -> SMARTWATCH_SERVICE.createAndSaveProducts(1);
+            default -> throw new IllegalArgumentException("Unknown Product type: " + type);
+        };
+    }
+
+}
