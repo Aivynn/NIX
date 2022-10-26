1ca52aa6718e120a7c138d054739754cb7f00684
Aivynn <aivyn1337@gmail.com>
2022-10-26 05:17:32 +0300

    FlyWayhomework

---
+++ src/main/resources/db/migration/V2.0.0__create_phone.sql
@@ -0,0 +1,6 @@
+insert into phone(id, count, price, title, type, date,manufacturer,model,invoice_id)
+	values ('ebb641c1-5f0f-4d8a-8daa-53e7bdcb2e33', 1, 4000, 'something','15','2022-10-26 20:15:24.036)',1,'k-65',1);
+insert into phone(id, count, price, title, type, date,manufacturer,model,invoice_id)
+	values ('ebb641c1-5f0f-4d8a-8daa-53e7bdcb2e34', 1, 4500, '1233','15','2022-10-26 20:15:24.036)',1,'k-62',1);
+insert into phone(id, count, price, title, type, date,manufacturer,model,invoice_id)
+	values ('ebb641c1-5f0f-4d8a-8daa-53e7bdcb2e35', 1, 5000, 'somet4124124hing','15','2022-10-26 20:15:24.036)',1,'k-63',1);
---
+++ src/main/resources/db/migration/V2.0.2__create_smartwatch.sql
@@ -0,0 +1,6 @@
+insert into smartwatch(id, count, price, title, type, date,manufacturer,model,invoice_id)
+	values ('ebb641c1-5f0f-4d8a-8daa-53e7bdcb2e36', 1, 4000, 'something','15','2022-10-26 20:15:24.036)',1,'k-65',1);
+insert into smartwatch(id, count, price, title, type, date,manufacturer,model,invoice_id)
+	values ('ebb641c1-5f0f-4d8a-8daa-53e7bdcb2e37', 1, 4500, '1233','15','2022-10-26 20:15:24.036)',1,'k-62',1);
+insert into smartwatch(id, count, price, title, type, date,manufacturer,model,invoice_id)
+	values ('ebb641c1-5f0f-4d8a-8daa-53e7bdcb2e38', 1, 5000, 'somet4124124hing','15','2022-10-26 20:15:24.036)',1,'k-63',1);
